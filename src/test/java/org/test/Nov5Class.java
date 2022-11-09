3c13c799ac79d90a48ca88f63045947ba82c1b04
harikrish09 <117385064+harikrish09@users.noreply.github.com>
2022-11-09 23:15:07 +0530

    Add files via upload

---
+++ src/test/java/org/test/Nov5Class.java
@@ -0,0 +1,16 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class Nov5Class {
+
+	@Test
+	private void Test() {
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		System.out.println("Test4");
+		System.out.println("Test5");
+	}
+	
+}
